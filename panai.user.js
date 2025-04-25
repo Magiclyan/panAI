fd82cbea82e28a83d2c8a56a9bfb87eb63eb64f9
Magiclyan <60562502+Magiclyan@users.noreply.github.com>
2025-04-25 10:39:59 +0800

    Update panai.user.js
    屏蔽已失效的老123云盘域名(123pan)
    添加新123云盘域名(123865、123684)

--- panai.user.js
+++ panai.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name              网盘智能识别助手
 // @namespace         https://github.com/syhyz1990/panAI
-// @version           2.0.2
+// @version           2.0.3
 // @author            YouXiaoHou,52fisher
-// @description       智能识别选中文字中的🔗网盘链接和🔑提取码，识别成功打开网盘链接并自动填写提取码，省去手动复制提取码在输入的烦恼。支持识别 ✅百度网盘 ✅阿里云盘 ✅腾讯微云 ✅蓝奏云 ✅天翼云盘 ✅移动云盘 ✅迅雷云盘 ✅123云盘 ✅360云盘 ✅115网盘 ✅奶牛快传 ✅城通网盘 ✅夸克网盘 ✅FlowUs息流 ✅Chrome 扩展商店 ✅Edge 扩展商店 ✅Firefox 扩展商店 ✅Windows 应用商店。
+// @description       智能识别选中文字中的🔗网盘链接和🔑提取码，识别成功打开网盘链接并自动填写提取码，省去手动复制提取码在输入的烦恼。支持识别 ✅百度网盘 ✅阿里云盘 ✅腾讯微云 ✅蓝奏云 ✅天翼云盘 ✅移动云盘 ✅迅雷云盘 ✅新123云盘 ✅360云盘 ✅115网盘 ✅奶牛快传 ✅城通网盘 ✅夸克网盘 ✅FlowUs息流 ✅Chrome 扩展商店 ✅Edge 扩展商店 ✅Firefox 扩展商店 ✅Windows 应用商店。
 // @license           AGPL-3.0-or-later
 // @homepage          https://www.youxiaohou.com/tool/install-panai.html
 // @supportURL        https://github.com/syhyz1990/panAI
@@ -167,13 +167,30 @@
             name: '迅雷云盘',
             storage: 'hash'
         },
-        '123pan': {
-            reg: /((?:https?:\/\/)?www\.123pan\.com\/s\/[\w-]{6,})/,
-            host: /www\.123pan\.com/,
-            input: ['.ca-fot input', ".appinput .appinput"],
-            button: ['.ca-fot button', ".appinput button"],
-            name: '123云盘',
-            storage: 'hash'
+//  -- 失效的域名 --        
+//      '123pan': {
+//          reg: /((?:https?:\/\/)?www\.123pan\.com\/s\/[\w-]{6,})/,
+//          host: /www\.123pan\.com/,
+//          input: ['.ca-fot input', ".appinput .appinput"],
+//          button: ['.ca-fot button', ".appinput button"],
+//          name: '123云盘',
+//          storage: 'hash'
+//        },
+        '123865': {
+           reg: /((?:https?:\/\/)?www\.123865\.com\/s\/[\w-]+(?:\?.*)?)/,
+           host: /www\.123865\.com/,
+           input: ['.ca-fot input', ".appinput .appinput"],
+           button: ['.ca-fot button', ".appinput button"],
+           name: '新123云盘',
+           storage: 'hash'
+        },
+       '123684': {
+           reg: /((?:https?:\/\/)?www\.123684\.com\/s\/[\w-]+(?:\?.*)?)/,
+           host: /www\.123684\.com/,
+           input: ['.ca-fot input', ".appinput .appinput"],
+           button: ['.ca-fot button', ".appinput button"],
+           name: '新123云盘-备用',
+           storage: 'hash'
         },
         '360': {
             reg: /((?:https?:\/\/)?(?:[a-zA-Z\d\-.]+)?(?:yunpan\.360\.cn|yunpan\.com)(\/lk)?\/surl_\w{6,})/,
